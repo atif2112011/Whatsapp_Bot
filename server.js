8ae8a7307f1f7f1f2314d1f758e57f26ee6bd025
Blaze1012 <atif251171@gmail.com>
2024-10-10 20:39:49 +0530

    Added cron job

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ bot.Startbot();
 app.use("/status", express.static(path.join(__dirname, "frontend")));
 app.use(express.json());
 app.use(BotRoutes);
+
+app.get('/ping',(req,res)=>{
+  try {
+    return res.send("Keep Alive Request Success");
+  } catch (error) {
+    return res.send(error.message)
+  }
+})
+
+//CRON
+// Ping the server every 25 minutes (300,000 ms)
+setInterval(() => {
+  axios.get('https://your-render-app-url/ping')
+      .then(response => console.log('Keep-alive request successful'))
+      .catch(error => console.log('Error sending keep-alive request:', error));
+}, 1500000); // 5 minutes in milliseconds
+
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
 });
